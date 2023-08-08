8b8d0f85e22374fc516a0dd175c0e72f8b437935
Kirin28 <kirin.kulikova@gmail.com>
2023-08-08 14:58:15 +0200

    maxdiff challenge

---
+++ maxDifference.js
@@ -0,0 +1,20 @@
+//Challenge: Write a function that takes an array of integers as input and returns the maximum difference between any two elements in the array.
+//this code challenge was created by ChatGPT and I'm not sure it gave the correct instructions
+function maxDifference(arr) {
+let diff = 0;
+let newAr = [];
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[i] > arr[j]) {
+            diff = arr[i] - arr[j];
+            newAr.push(diff);
+        } else {
+            diff = 0;
+        }
+    }
+}
+return newAr.reduce((a, b) => Math.max(a, b), -Infinity);
+} 
+
+console.log(maxDifference([4, 2, 9, 7, 5])); //7
+console.log(maxDifference([1, 2, 3, 4, 5])); //4
